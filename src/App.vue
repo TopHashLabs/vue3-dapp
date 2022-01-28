c7fc7174673ab239f85a4a559a3fd8e43b5863b0
sarmat <skhachirov12@gmail.com>
2022-01-28 23:48:54 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import BaseButton from './components/base/BaseButton.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1>Vue3 + Vite + TailwindCSS dApp Template</h1>
+    <BaseButton color="blue" size="lg">button</BaseButton>
+  </div>
+</template>
+
+<style></style>
---
+++ src/components/base/BaseButton.vue
@@ -0,0 +1,12 @@
+<template>
+  <component :is="rootTag" class="p-2 bg-blue-500 text-white rounded-md">
+    <slot></slot>
+  </component>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+const rootTag = computed(() => 'button')
+</script>
+
+<style></style>
