360ff48fd73871ad54dabe53248f181de0ce4cee
Alan Nabiev <alannabiev2001@gmail.com>
2022-02-11 18:55:20 +0300

    feat: global register components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import path from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import ViteComponents from 'unplugin-vue-components/vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  define: {
+    'process.env': process.env
+  },
+  plugins: [
+    vue({ reactivityTransform: true }),
+    ViteComponents(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    },
+  },
+  optimizeDeps: {
+    // @ts-ignore
+    allowNodeBuiltins: ['stream']
+  }
+});
