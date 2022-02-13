6f7262d0214414ded4e5056782d0fb84c048d5b5
sarmat <skhachirov12@gmail.com>
2022-02-13 16:20:11 +0300

    feat: wallet login

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <script setup lang="ts">
+import { ref, provide } from 'vue'
+import { useWeb3 } from '@/composables/useWeb3'
+import { shorten } from './helpers/utils'
+
+const { login, web3 } = useWeb3()
+
+provide('web3', web3)
+
+const loading = ref(false)
+
+async function handleLogin() {
+  loading.value = true
+  await login('injected')
+  loading.value = false
+}
 </script>
 
 <template>
   <div class="container">
-    <h1>Vue3 + Vite + TailwindCSS dApp Template</h1>
-    <BaseButton color="white" loading>button</BaseButton>
+    <BaseButton @click="handleLogin" :loading="loading"
+      >Connect Wallet</BaseButton
+    >
+    <div>
+      <h1>
+        Connected with: <span>{{ shorten(web3.account) }}</span>
+      </h1>
+    </div>
   </div>
 </template>
 
